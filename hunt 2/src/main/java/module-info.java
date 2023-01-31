0faa94ee71866a6d1684b42101670a9f69b392f2
Dmytro-Romaniv <52633228+Dmytro-Romaniv@users.noreply.github.com>
2023-01-31 14:36:06 +0100

    mistake

--- hunt 2/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module oop.hunt {
-    requires javafx.controls;
-//    requires javafx.fxml;
-    requires javafx.media;
-
-    opens oop.hunt to javafx.fxml;
-    exports oop.hunt;
-}
--- hunt 2/src/main/java/oop/hunt/Launcher.java
+++
@@ -1,7 +0,0 @@
-package oop.hunt;
-
-    public class Launcher {
-    public static void main(String[] args) {
-        Main.main(args);
-    }
-}
